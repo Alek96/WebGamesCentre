e08c9fe6c5bd5ae60a9534c58aabb2acb4433eb2
Aleksander Zamojski <zamojski96@gmail.com>
2017-04-24 15:41:52 +0200

    first version of SConstruct.py that does not work

--- build/SConstruct.py
+++ build/SConstruct.py
@@ -4,7 +4,25 @@
 import SCons.Script
 from SCons.Environment import Environment
 
+import os.path
+
+PocoBase = "C:\\Libraries\\poco-1.7.8p2\\"
+WGCProjectBase= "C:\\Users\\Aleksander\\Documents\\GitHub\\WebGamesCentre"
+
 print "Building Server"
 
-env = Environment()   # Create an environmnet
-env.Program(target = "scons_test", source = ["scons_test.c"])
+env = Environment(TARGET_ARCH= 'x86')   # Create an environmnet
+
+Headers = ['Foundation\\include' , 'Util\\include', 'Net\\include', 'JSON\\include']
+headers = [os.path.join(PocoBase, x) for x in Headers]
+
+#$(PocoBase)\Util\include;$(PocoBase)\Net\include;$(PocoBase)\JSON\include</AdditionalIncludeDirectories>]
+
+env.Program(target = "server", source = WGCProjectBase + "/src/Server.cpp", CPPPATH = [WGCProjectBase] + headers, LIBS = ['PocoFoundationd.lib', 'PocoNetd.lib', 'PocoUtild.lib',
+'PocoXMLd.lib', 'PocoJSONd.lib'	], LIBPATH = PocoBase + "\\lib" )
+
+#import os.path
+#filenames = [os.path.join(prefix, x) for x in filenames]
+
+#<AdditionalIncludeDirectories>$(WGCProjectBase);$(PocoBase)\Foundation\include;$(PocoBase)\Util\include;$(PocoBase)\Net\include;$(PocoBase)\JSON\include</AdditionalIncludeDirectories>
+#<AdditionalLibraryDirectories>$(PocoBase)\lib</AdditionalLibraryDirectories>
