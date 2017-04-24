1d88b8c9bf06b11be459d6c9eedad8ed9ab9512a
Aleksander Zamojski <zamojski96@gmail.com>
2017-04-24 21:21:53 +0200

    VS can build project from SCons

---
+++ VSProject/WGCServer/SConstruct-test.py
@@ -0,0 +1,10 @@
+#!python
+
+# These import lines are not really needed, but it helps intellisense within VS when editing the script
+import SCons.Script
+from SCons.Environment import Environment
+
+print "Building scons_test"
+
+env = Environment()   # Create an environmnet
+env.Program(target = "WGCServer", source = ["scons_test.c"])
