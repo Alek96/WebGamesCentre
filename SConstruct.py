d2ecadea87688136824124c9eccfb62b9708b298
Aleksander Zamojski <zamojski96@gmail.com>
2017-04-25 09:42:10 +0200

    Repair debug (it is default variable during build)

--- SConstruct.py
+++ SConstruct.py
@@ -16,18 +16,20 @@ env = Environment(TARGET_ARCH= 'x86')   # Create an environmnet for 32 bit versi
 platform = ARGUMENTS.get('OS', Platform())
 print platform
 
+LibS = ['']
 # Add the Debug Flags if debug=1 is specified on the command line, this tends to be compiler specific
-if ARGUMENTS.get('debug') == '1':
+if ARGUMENTS.get('debug') != '0':
     env.Append(CPPDEFINES = ['DEBUG', '_DEBUG'])
     env.Append(CCFLAGS='/MDd')
     #env.Append(CCFLAGS=Split('/Zi /Fd${TARGET}.pdb'))
     env.Append(LINKFLAGS = ['/DEBUG'])
     variant = 'Debug'
-	#libS = Split('PocoFoundationd PocoNetd PocoUtild PocoXMLd PocoJSONd')
+    #env.VariantDir('VSProject/Debug/', 'src', duplicate=0)	#not working ???
+    libS = Split('PocoFoundationd PocoNetd PocoUtild PocoXMLd PocoJSONd')
 else:
     env.Append(CPPDEFINES = ['NDEBUG'])
     variant = 'Release'
-	#libS = Split('PocoFoundation PocoNet PocoUtil PocoXML PocoJSON')
+    libS = Split('PocoFoundation.lib PocoNet.lib PocoUtil.lib PocoXML.lib PocoJSON.lib')
 
 
 print "Building: " + variant
@@ -39,22 +41,23 @@ src_files = Split('''src/PageRequestHandler.cpp
 					 src/Server.cpp
 					 src/WebSocketRequestHandler.cpp''')
 
-PocoHeaders = Split('''\\Foundation\\include
-					   \\Util\\include
-					   \\Net\\include
-					   \\JSON\\include''')
+PocoHeaders = Split('''/Foundation/include
+					   /Util/include
+					   /Net/include
+					   /JSON/include''')
 PocoHeaders = [PocoBase + x for x in PocoHeaders]
 
 
 t = env.Program(target = 'VSProject/Debug/WGCServer', 
 				source = src_files, 
 				CPPPATH = [WGCProjectBase] + PocoHeaders,
-				LIBS = Split('''PocoFoundationd 
-								PocoNetd
-								PocoUtild 
-								PocoXMLd 
-								PocoJSONd'''), 
-				LIBPATH = PocoBase + '\\lib' )
+				#LIBS = Split('''PocoFoundationd 
+				#				PocoNetd
+				#				PocoUtild 
+				#				PocoXMLd 
+				#				PocoJSONd'''),
+				LIBS = LibS,
+				LIBPATH = PocoBase + '/lib' )
 
 
 Default(t)
