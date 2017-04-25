f16302362915da0d6e3ce72fc606d3a9d9d726f9
Peperzastey <przemyslaw.poljanski@gmail.com>
2017-04-26 01:33:08 +0200

    Add more tests for RequestHandlerFactory
    Repair exception-throwing when creating loggerDummy second time.
    Add new entry related to tests VS project to .gitignore.

--- tests/RequestHandlerFactoryTest.cpp
+++ tests/RequestHandlerFactoryTest.cpp
@@ -11,21 +11,105 @@
 using namespace Poco::Net;
 using namespace Catch;
 
-SCENARIO("RequestHandlerFactory returns appropriate RequestHandler", "[RequestHandlers]") {
+SCENARIO("RequestHandlerFactory returns appropriate RequestHandler", "[RequestHandlers]")
+{
+	// neccessary setup (dummies)
+	Poco::AutoPtr<Poco::NullChannel> channelDummyPtr = new Poco::NullChannel;
+	Poco::Logger& loggerDummy = Poco::Logger::get("dummy");
+	loggerDummy.setChannel(channelDummyPtr);
 
-	GIVEN("A RequestHandlerFactory") {
-		Poco::AutoPtr<Poco::NullChannel> channelDummyPtr = new Poco::NullChannel;
-		Poco::Logger& loggerDummy = Poco::Logger::create("dummy", channelDummyPtr);
+	GIVEN("A RequestHandlerFactory object")
+	{
 		RequestHandlerFactory requestFactory("dummy", loggerDummy);
 		HTTPServerRequestDummy request;
 
-		WHEN("requested URI ends with '.css'") {
+		WHEN("Requested URI ends with '.css'") 
+		{
 			request.setURI("moredummies.css");
+			Poco::SharedPtr<PageRequestHandler> requestHandlerPtr = dynamic_cast<PageRequestHandler*>(requestFactory.createRequestHandler(request));
 
-			THEN("createRequestHandler method returns a PageRequestHandler method with ContentType == 'text/css'") {
-				Poco::SharedPtr<PageRequestHandler> requestHandlerPtr = dynamic_cast<PageRequestHandler*>(requestFactory.createRequestHandler(request));
+			THEN("createRequestHandler method returns a PageRequestHandler object")
+			{				
 				REQUIRE(requestHandlerPtr.get() != nullptr);
+			}
+			THEN("PageRequestHandler object returned by createRequestHandler method has ContentType == 'text/css'")
+			{
 				REQUIRE_THAT(requestHandlerPtr->getContentType(), Equals("text/css"));
+			}
+		}
+
+		WHEN("Requested URI ends with '.js'")
+		{
+			request.setURI("moredummies.js");
+			Poco::SharedPtr<PageRequestHandler> requestHandlerPtr = dynamic_cast<PageRequestHandler*>(requestFactory.createRequestHandler(request));
+
+			THEN("createRequestHandler method returns a PageRequestHandler object")
+			{
+				REQUIRE(requestHandlerPtr.get() != nullptr);
+			}
+			THEN("PageRequestHandler object returned by createRequestHandler method has ContentType == 'text/javascript'")
+			{
+				REQUIRE_THAT(requestHandlerPtr->getContentType(), Equals("text/javascript"));
+			}
+		}
+
+		WHEN("Requested URI ends with '.html'")
+		{
+			request.setURI("moredummies.html");
+			Poco::SharedPtr<PageRequestHandler> requestHandlerPtr = dynamic_cast<PageRequestHandler*>(requestFactory.createRequestHandler(request));
+
+			THEN("createRequestHandler method returns a PageRequestHandler object")
+			{
+				REQUIRE(requestHandlerPtr.get() != nullptr);
+			}
+			THEN("PageRequestHandler object returned by createRequestHandler method has ContentType == 'text/html'")
+			{
+				REQUIRE_THAT(requestHandlerPtr->getContentType(), Equals("text/html"));
+			}
+		}
+
+		WHEN("Requested URI ends with '.htm'")
+		{
+			request.setURI("moredummies.htm");
+			Poco::SharedPtr<PageRequestHandler> requestHandlerPtr = dynamic_cast<PageRequestHandler*>(requestFactory.createRequestHandler(request));
+
+			THEN("createRequestHandler method returns a PageRequestHandler object")
+			{
+				REQUIRE(requestHandlerPtr.get() != nullptr);
+			}
+			THEN("PageRequestHandler object returned by createRequestHandler method has ContentType == 'text/html'")
+			{
+				REQUIRE_THAT(requestHandlerPtr->getContentType(), Equals("text/html"));
+			}
+		}
+
+		WHEN("Requested URI is '/'")
+		{
+			request.setURI("/");
+			Poco::SharedPtr<PageRequestHandler> requestHandlerPtr = dynamic_cast<PageRequestHandler*>(requestFactory.createRequestHandler(request));
+
+			THEN("createRequestHandler method returns a PageRequestHandler object")
+			{
+				REQUIRE(requestHandlerPtr.get() != nullptr);
+			}
+			THEN("PageRequestHandler object returned by createRequestHandler method has ContentType == 'text/html'")
+			{
+				REQUIRE_THAT(requestHandlerPtr->getContentType(), Equals("text/html"));
+			}
+		}
+
+		WHEN("Requested URI ends with none of: '.html', '.htm', '.css', '.js'; and is not: '/'. For example ends with '.x'")
+		{
+			request.setURI("moredummies.x");
+			Poco::SharedPtr<PageRequestHandler> requestHandlerPtr = dynamic_cast<PageRequestHandler*>(requestFactory.createRequestHandler(request));
+
+			THEN("createRequestHandler method returns a PageRequestHandler object")
+			{
+				REQUIRE(requestHandlerPtr.get() != nullptr);
+			}
+			THEN("PageRequestHandler object returned by createRequestHandler method has ContentType == ''")
+			{
+				REQUIRE_THAT(requestHandlerPtr->getContentType(), Equals(""));
 			}
 		}
 		
--- tests/catchTest.cpp
+++ tests/catchTest.cpp
@@ -1,10 +1,12 @@
 #include "catch-framework/catch.hpp"
 
-unsigned int Factorial(unsigned int number) {
+unsigned int Factorial(unsigned int number)
+{
 	return number <= 1 ? 1 : Factorial(number - 1)*number;
 }
 
-TEST_CASE("Factorials are computed", "[factorial]") {
+TEST_CASE("Factorials are computed", "[factorial]")
+{
 	REQUIRE(Factorial(0) == 1);
 	REQUIRE(Factorial(1) == 1);
 	REQUIRE(Factorial(2) == 2);
